ad957e872c8a86ab6cb629241e93d0eda837c031
kumarvimal0503 <kumar.vimal0503@gmail.com>
2017-10-06 08:56:34 +0530

    ff

--- CoreJavaDemoAnvay/src/MethodOverloadDemo.java
+++ CoreJavaDemoAnvay/src/MethodOverloadDemo.java
@@ -1,16 +1,28 @@
 
 public class MethodOverloadDemo {
-	public void add(int a, int b){
+	public static void add(int a, int b){
+		
+		System.out.println("Add Integer 2 param");
 		System.out.println((a + b));
 	}
 	
 	
-	public void add(int a, int b, int c){
+	public static void add(int a, int b, int c){
+		System.out.println("Add Integer 3 param");
 		System.out.println((a + b + c));
 	}
 	
 	
-	public void add(int a, float b){
+	public static void add(float a, float b){
+		System.out.println("Add float 2 param");
 		System.out.println((a + b));
+	}
+	
+	
+	public static void main(String[]args) {
+		
+		add(10, 20);
+		add(10, 20, 30);
+		add(10.1f, 20.2f);
 	}
 }
